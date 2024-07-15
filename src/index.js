93b40818fe8d3ae56c4d13baeddc14199964f42a
tptam <99264397+tptam@users.noreply.github.com>
2024-07-16 00:08:49 +0900

    Add test code for Todo/TodoViewModels

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import Todo from "./todo.js"
+import TodoViewModel from "./todo-view-model.js"
 import "./style.css";
 
-console.log('hi');
+const todo = new Todo(
+    "Visit Burt",
+    "Discuss our work environment. Address: 3329 Gull Harbor Rd. Kier, PE 07453",
+    (new Date('January 16, 2025 21:00:00')),
+    3
+);
+
+const todoViewModel = new TodoViewModel(todo);
+
+console.log(todoViewModel.toString());
