b49035bf09bc34a78d94e710079f2111aa973203
tptam <99264397+tptam@users.noreply.github.com>
2024-07-16 13:34:19 +0900

    Add data rules to todo.js

--- src/todo.js
+++ src/todo.js
@@ -1,15 +1,17 @@
+import { isAfter, isToday } from "date-fns";
+
 class Todo {
     #title;
     #description;
     #dueDate;
     #priority;
     #done;
-    constructor(title, description, dueDate, priority) {
+    constructor(title, description, dueDate, priority, done) {
         this.#title = title;
         this.#description = description;
         this.#dueDate = dueDate;
         this.#priority = priority;
-        this.#done = false;
+        this.#done = done;
     }
     get title(){
         return this.#title;
@@ -40,6 +42,40 @@ class Todo {
     }
     set done(newDone) {
         this.#done = newDone;
+    }
+
+    static rules = {
+        title: [
+            val => val !== null,
+            val => val !== undefined,
+            val => typeof (val) === 'string',
+            val => val.length < 200
+        ],
+        description: [
+            val => val !== null,
+            val => val !== undefined,
+            val => typeof (val) === 'string',
+        ],
+        dueDate: [
+            val => val !== null,
+            val => val !== undefined,
+            val => Date.prototype.isPrototypeOf(val),
+            val => {
+                const today = new Date();
+                return isAfter(val, today) || isToday(val);
+            },
+        ],
+        priority: [
+            val => val !== null,
+            val => val !== undefined,
+            val => typeof(val) === "number",
+            val => [3,2,1,0].includes(val),
+        ],
+        done: [
+            val => val !== null,
+            val => val !== undefined,
+            val => typeof (val) === "boolean",
+        ],
     }
 }
 
