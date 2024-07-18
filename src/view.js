78d6c536533cd74eaa270779949eabbdd74df27f
tptam <99264397+tptam@users.noreply.github.com>
2024-07-18 11:03:35 +0900

    Modify view to import todosTable and todosTableRow

--- src/view.js
+++ src/view.js
@@ -1,5 +1,7 @@
 import * as todos from "./todos-view";
+import * as todosTable from "./todos-table-view";
+import * as todosTableRow from "./todos-table-row-view";
 import * as menu from "./menu-view";
 
 
-export { todos, menu };
+export { todos, todosTable, todosTableRow, menu };
