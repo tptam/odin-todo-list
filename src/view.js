f3c15b2cd8c3072baaf58f0816c39ab366298044
tptam <99264397+tptam@users.noreply.github.com>
2024-07-19 13:41:24 +0900

    Add projects view to barrel items

--- src/view.js
+++ src/view.js
@@ -3,6 +3,10 @@ import * as todosTable from "./todos-table-view";
 import * as todosTableRow from "./todos-table-row-view";
 import * as statusButton from "./todo-status-button-view";
 import * as menu from "./menu-view";
+import * as projects from "./projects-view";
 
-
-export { todos, todosTable, todosTableRow, statusButton, menu };
+export { 
+    todos, todosTable, todosTableRow, statusButton, 
+    menu,
+    projects,
+};
