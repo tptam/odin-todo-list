2d1bfae5394966db5d4edc6292c97da76160b804
tptam <99264397+tptam@users.noreply.github.com>
2024-07-20 22:53:44 +0900

    Add todoEdit to barrel items

--- src/view.js
+++ src/view.js
@@ -5,10 +5,11 @@ import * as statusButton from "./todo-status-button-view";
 import * as menu from "./menu-view";
 import * as projects from "./projects-view";
 import * as todoAdd from "./todo-add-view";
+import * as todoEdit from "./todo-edit-view";
 
 export { 
     todos, todosTable, todosTableRow, statusButton, 
     menu,
     projects,
-    todoAdd,
+    todoAdd, todoEdit,
 };
