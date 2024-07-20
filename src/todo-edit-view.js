3a798445a415aa4443b6e38920e2ed7d0796cb33
tptam <99264397+tptam@users.noreply.github.com>
2024-07-21 00:05:36 +0900

    Add status button

--- src/todo-edit-view.js
+++ src/todo-edit-view.js
@@ -1,12 +1,36 @@
 import * as modalView from "./modal-view";
 import * as todoFormView from "./todo-form-view";
+import * as statusButtonView from "./todo-status-button-view"
 
 function render(content, formJson, formHandlers) {
+    const formData = JSON.parse(formJson);
     const dialog = document.createElement("dialog");
     content.appendChild(dialog)
     modalView.render(dialog, "Edit ToDo Task", formHandlers);
     todoFormView.render(dialog, formJson, formHandlers);
-    dialog.querySelector("form").setAttribute("name", "todoAdd");
+
+    const form = dialog.querySelector("form");
+    form.setAttribute("name", "todoEdit");
+
+    const title = form.title;
+    const dueDate = form.dueDate;
+    const priority = form.priority;
+    const urgent = form.urgent;
+    const important = form.important;
+    const description = form.description;
+    const project = form.project;
+
+    const button = document.createElement("button");
+    button.classList.add("status");
+    statusButtonView.render(
+        button, 
+        JSON.stringify({
+            id: formData.todo.id,
+            done: formData.todo.done,
+        }), 
+        formHandlers.clickStatusButton
+    );
+    form.insertBefore(button, dueDate.parentNode);
 }
 
 export { render };
